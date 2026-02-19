81a33d42cda2d9b8480f2a812bb2f2ee0330262c
Yousef-116 <yousef.com1162003@gmail.com>
2026-02-19 13:43:19 +0200

    Enhance product details and shop item components: add dynamic quantity management, discount calculation, and integrate Toastr notifications for basket actions.

--- src/app/shop/product-details/product-details.component.ts
+++ src/app/shop/product-details/product-details.component.ts
@@ -1,21 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { ShopService } from '../shop.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { IProduct } from '../../Models/Product';
 import { CurrencyPipe } from '@angular/common';
 import { NgxImageZoomModule } from 'ngx-image-zoom';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { BasketService } from '../../basket/basket.service';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [CurrencyPipe, NgxImageZoomModule],
+  imports: [CurrencyPipe, NgxImageZoomModule, ToastrModule, RouterLink],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss',
 })
 export class ProductDetailsComponent implements OnInit {
+  CalculateDiscountPercentage(oldPrice: number, newPrice: number) {
+    if (oldPrice === 0) return 0;
+    return Math.round(((oldPrice - newPrice) / oldPrice) * 100).toFixed(1);
+  }
+  SetBasketValue() {
+    console.log('Adding to basket: ', this.product.name);
+    this.basketService.addItemToBasket(this.product, this.Quantity);
+    this.toastr.success('Item added to basket', 'Success');
+  }
+  DecreaseQuantity() {
+    if (this.Quantity > 1) {
+      this.Quantity--;
+      this.toastr.info('Quantity decreased', 'Info');
+    } else {
+      this.toastr.warning('Minimum quantity reached', 'Warning');
+    }
+  }
+  IncreaseQuantity() {
+    if (this.Quantity < 10) {
+      this.Quantity++;
+      this.toastr.success('Quantity increased', 'Success');
+    } else {
+      this.toastr.warning('Maximum quantity reached', 'Warning');
+    }
+  }
+
   constructor(
     private shopService: ShopService,
     private router: Router,
+    private toastr: ToastrService,
+    private basketService: BasketService,
   ) {}
   id: number;
   product: IProduct;
--- src/app/shop/shop-item/shop-item.component.ts
+++ src/app/shop/shop-item/shop-item.component.ts
@@ -3,21 +3,26 @@ import { Component, Input } from '@angular/core';
 import { IProduct } from '../../Models/Product';
 import { RouterModule } from '@angular/router';
 import { BasketService } from '../../basket/basket.service';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-shop-item',
   standalone: true,
-  imports: [CurrencyPipe, RouterModule],
+  imports: [CurrencyPipe, RouterModule, ToastrModule],
   templateUrl: './shop-item.component.html',
   styleUrls: ['./shop-item.component.scss'],
 })
 export class ShopItemComponent {
   @Input({ required: true }) product!: IProduct;
 
-  constructor(private service: BasketService) {}
+  constructor(
+    private service: BasketService,
+    private toastr: ToastrService,
+  ) {}
   SetBasketValue() {
     console.log('Adding to basket: ', this.product.name);
     this.service.addItemToBasket(this.product);
+    this.toastr.success('Item added to basket', 'Success');
   }
   //   constructor(private _service: BasketService) {}
   //   @Input() Product: IProduct;
